09132c8aa2ebef860da7813bd2f93e17a3b4d4f6
Marcos Venicius <marcos@devone.dev.br>
2023-05-15 20:56:33 -0300

    configure basic prisma

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,16 @@
 import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
 
 const app = fastify()
+const prisma = new PrismaClient()
 
 const PORT = process.env.PORT || 3333
+
+app.get('/users', async () => {
+  const users = await prisma.user.findMany()
+
+  return users
+})
 
 app
   .listen({
