38d9f301eb48e852809ca5e5507a466293270f7d
Marcos Venicius <marcos@devone.dev.br>
2023-05-16 19:51:46 -0300

    implement web homepage

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,11 +1,20 @@
 import "./globals.css";
-import { Inter } from "next/font/google";
+import {
+  Roboto_Flex as Roboto,
+  Bai_Jamjuree as BaiJamjuree,
+} from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
+const roboto = Roboto({ subsets: ["latin"], variable: "--font-roboto" });
+const baiJamjuree = BaiJamjuree({
+  subsets: ["latin"],
+  weight: "700",
+  variable: "--font-bai-jamjuree",
+});
 
 export const metadata = {
   title: "Linha do tempo",
-  description: "Minha linha do tempo",
+  description:
+    "Uma cápsula do tempo construida com React, NextJS, TailwindCSS e Typescript",
 };
 
 export default function RootLayout({
@@ -15,7 +24,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt-BR">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={`${roboto.variable} ${baiJamjuree.variable} font-sans text-gray-100 bg-gray-900`}
+      >
+        {children}
+      </body>
     </html>
   );
 }
--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,3 +1,75 @@
+import { User } from "lucide-react";
+
+import logo from "../assets/logo.svg";
+import Image from "next/image";
+
 export default function Home() {
-  return <h1>Hello world</h1>;
+  return (
+    <main className="grid grid-cols-2 min-h-screen">
+      <div className="flex flex-col items-start justify-between px-28 py-16 relative overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)] bg-cover">
+        <div className="absolute right-0 top-1/2 -translate-y-1/2 translate-x-1/2 h-[288px] w-[526px] bg-purple-700 rounded-full opacity-50 blur-full" />
+
+        <div className="absolute right-2 top-0 bottom-0 w-2 bg-stripes" />
+
+        <a
+          href="#"
+          className="flex items-center gap-3 text-left hover:text-gray-50 transition-colors"
+        >
+          <div className="flex h-10 w-10 items-center justify-center rounded-full bg-gray-400">
+            <User className="h-5 w-5 text-gray-500" />
+          </div>
+
+          <p className="text-sm leading-snug max-w-[140px]">
+            <span className="underline">Crie sua conta</span> e salve suas
+            memórias!
+          </p>
+        </a>
+
+        <div className="space-y-5">
+          <Image src={logo} alt="NLW spacetime logo" />
+
+          <div className="max-w-[420px] space-y-1">
+            <h1 className="text-5xl font-bold leading-tight text-gray-50">
+              Sua cápsula do tempo
+            </h1>
+
+            <p className="text-lg leading-relaxed">
+              Colecione momentos marcantes da sua jornada e compartilhe (se
+              quiser) com o mundo!
+            </p>
+          </div>
+
+          <a
+            href="#"
+            className="inline-block rounded-full bg-green-500 px-5 py-3 font-alt text-sm uppercase leading-none text-black hover:bg-green-600 transition-colors"
+          >
+            CADASTRAR LEMBRANÇA
+          </a>
+        </div>
+
+        <p className="text-sm leading-relaxed text-gray-200">
+          Feito com 💜 no NLW da{" "}
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://rocketseat.com.br"
+            className="underline hover:text-gray-100 transition-colors"
+          >
+            Rocketseat
+          </a>
+        </p>
+      </div>
+
+      <div className="flex flex-col p-16 bg-[url(../assets/bg-stars.svg)] bg-cover">
+        <div className="flex flex-1 items-center justify-center">
+          <p className="text-center leading-relaxed w-[360px]">
+            Você ainda não registrou nenhuma lembrança, comece a{" "}
+            <a href="#" className="underline hover:text-gray-50">
+              criar agora!
+            </a>
+          </p>
+        </div>
+      </div>
+    </main>
+  );
 }
