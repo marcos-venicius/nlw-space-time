168ccfc74ff98428008c1a793ca55fd61b8ef686
Marcos Venicius <marcos@devone.dev.br>
2023-05-15 21:21:40 -0300

    setup web app

--- README.md
+++ README.md
@@ -5,3 +5,8 @@ The NLW Space Time
 ### Links
 
 - [Figma (Layout)](<https://www.figma.com/file/LI0wcGClSaX0s8fiAPpOe5/C%C3%A1psula-do-tempo-%E2%80%A2-Trilha-Ignite-(Community)?type=design&node-id=213%3A22&t=eCja9DObpIit1cFt-1>)
+
+
+### Projects
+
+- [Server](./server/)
---
+++ web/src/app/layout.tsx
@@ -0,0 +1,21 @@
+import "./globals.css";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Linha do tempo",
+  description: "Minha linha do tempo",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="pt-BR">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
