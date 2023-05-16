b86fe7aac70251a0818989e6934f0f8b23271e2f
Marcos Venicius <marcos@devone.dev.br>
2023-05-16 17:57:16 -0300

    configure tailwind css

---
+++ mobile/App.tsx
@@ -0,0 +1,11 @@
+import { StatusBar } from "expo-status-bar";
+import { Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View className="flex items-center justify-center h-full">
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
