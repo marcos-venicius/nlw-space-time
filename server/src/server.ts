6e51637cc225161a42634f3eb6829bc36b360d43
Marcos Venicius <marcos@devone.dev.br>
2023-05-17 20:33:37 -0300

    register a github account

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,20 +1,25 @@
+import 'dotenv/config'
+
 import fastify from 'fastify'
 import cors from '@fastify/cors'
 import { memoriesRoutes } from './routes/memories'
+import { authRoutes } from './routes/auth'
 
 const app = fastify()
 
 app.register(cors, {
-  origin: true,
+  origin: true
 })
+
 app.register(memoriesRoutes)
+app.register(authRoutes)
 
 const PORT = process.env.PORT || 3333
 
 app
   .listen({
-    port: Number(PORT),
+    port: Number(PORT)
   })
-  .then((host) => {
+  .then(host => {
     console.log(`‣ 🚀 ${host.replace('[::1]', 'localhost')}`)
   })
