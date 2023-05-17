3f7944aeda10e1f0351802680b172259c490a273
Marcos Venicius <marcos@devone.dev.br>
2023-05-17 20:12:02 -0300

    componentize application

---
+++ web/src/components/copyright.tsx
@@ -0,0 +1,15 @@
+export function Copyright() {
+  return (
+    <p className="text-sm leading-relaxed text-gray-200">
+      Feito com 💜 no NLW da{" "}
+      <a
+        target="_blank"
+        rel="noreferrer"
+        href="https://rocketseat.com.br"
+        className="underline hover:text-gray-100 transition-colors"
+      >
+        Rocketseat
+      </a>
+    </p>
+  );
+}
