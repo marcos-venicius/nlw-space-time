728cbcece8fe5426cbf1f8d0b9368fd8c2871258
Marcos Venicius <marcosdevelopment.business@gmail.com>
2023-05-19 06:15:52 -0300

    server: create file upload feature

---
+++ server/src/routes/v1/file-upload.ts
@@ -0,0 +1,59 @@
+import { randomUUID } from 'node:crypto'
+import { FastifyInstance } from 'fastify'
+import { extname, resolve } from 'node:path'
+import { createWriteStream } from 'node:fs'
+import { pipeline } from 'node:stream'
+import { promisify } from 'node:util'
+
+const maxFileSize = 5 * 1024 * 1024
+
+const pump = promisify(pipeline)
+
+export async function fileUploadRoutes(app: FastifyInstance) {
+  // migrate to cloud flare r2
+  app.post('/image-upload', async (request, response) => {
+    const file = await request.file({
+      limits: {
+        fileSize: 5 * 1024 * 1024 // 5mb
+      }
+    })
+
+    if (!file) {
+      return response.status(400).send()
+    }
+
+    if (
+      file.file.bytesRead > maxFileSize ||
+      Number(request.headers['content-length']) > maxFileSize
+    ) {
+      return response.status(413).send({
+        message: `your image is very powerful, we only support up to 5 megabytes.`
+      })
+    }
+
+    const isImageRegex = /^image\/[a-z-A-Z]+$/
+    const fileIsAnImage = isImageRegex.test(file.mimetype)
+
+    if (!fileIsAnImage) {
+      return response.status(415).send({
+        message: `this type of file is not allowed`
+      })
+    }
+
+    const fileId = randomUUID()
+    const extension = extname(file.filename)
+
+    const filename = fileId.concat(extension)
+
+    const writeStream = createWriteStream(
+      resolve(__dirname, '..', '..', '..', 'uploads/', filename)
+    )
+
+    await pump(file.file, writeStream)
+
+    const fullUrl = request.protocol.concat('://').concat(request.hostname)
+    const fileUrl = new URL(`/uploads/${filename}`, fullUrl)
+
+    return fileUrl
+  })
+}
--- server/src/server.ts
+++ server/src/server.ts
@@ -2,19 +2,29 @@ import 'dotenv/config'
 import jwt from '@fastify/jwt'
 
 import fastify from 'fastify'
+import multipart from '@fastify/multipart'
 import cors from '@fastify/cors'
+import fastifyStatic from '@fastify/static'
 import { memoriesRoutesV1 } from './routes/v1/memories'
 import { authRoutesV1 } from './routes/v1/auth'
+import { fileUploadRoutes } from './routes/v1/file-upload'
+import { resolve } from 'node:path'
 
 const app = fastify()
 
+app.register(fastifyStatic, {
+  root: resolve(__dirname, '..', 'uploads'),
+  prefix: '/uploads'
+})
 app.register(cors, {
   origin: true
 })
+app.register(multipart)
 app.register(jwt, {
   secret: String(process.env.JWT_SECRET)
 })
 
+app.register(fileUploadRoutes, { prefix: '/api/v1' })
 app.register(memoriesRoutesV1, { prefix: '/api/v1' })
 app.register(authRoutesV1, { prefix: '/api/v1' })
 
